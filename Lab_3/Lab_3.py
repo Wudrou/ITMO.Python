b3076b6d43c60e7af3f4914968a3b5da3af52437
Wudrou <73528461+Wudrou@users.noreply.github.com>
2021-06-06 12:17:59 +0300

    Add Lab_3

--- Lab_3/Lab_3.py
+++ Lab_3/Lab_3.py
@@ -1,1 +1,16 @@
+import math
+import statistics
 
+#print(help(math))
+#print(help(statistics))
+
+list = [14, 43, 27, 1, -9, 56, 90, 11, 9, 176]
+print(list)
+print("Сумма чисел: ", math.fsum(list))
+print("Среднее значение: ", statistics.mean(list))
+print("Медиана: ", statistics.median(list))
+print("Стандартное отклонение: ", statistics.stdev(list))
+
+import random
+
+print("Случайное число от 1 до 100: ", random.randint(1, 100))
